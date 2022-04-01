9dcc0737194ee5384dc1feaea10fcb79a09ba177
Carolina Hoeflich <90994922+Carohoeflich@users.noreply.github.com>
2022-03-31 18:45:35 -0600

    Sesion1 Postwork
    Sesion1 Postwork

--- sesion1/src/App.js
+++ sesion1/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import React from 'react';
 import Foco from './ejemplo/Foco';
+import FilaFocos from './ejemplo/FilaFocos';
+import FocoMercurial from './ejemplo/FocoMercurial';
+import Fila3Focos from './ejemplo/Fila3Focos';
 
 function App() {
 
   return (
     <div id="techo">
-        <Foco></Foco>
-        <Foco></Foco>
-        <Foco></Foco>
-        <Foco></Foco>
+    <Foco></Foco>
+    <FocoMercurial></FocoMercurial>
+    <FilaFocos></FilaFocos>
+        <FocoMercurial></FocoMercurial>
+    <Fila3Focos></Fila3Focos>
     </div>
   );
 }
---
+++ sesion1/src/ejemplo/Fila3Focos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Foco from './Foco';
+
+const Fila3Focos = () => {
+   return (
+      <div className="filaFocos">
+         <Foco></Foco>
+         <Foco></Foco>
+         <Foco></Foco>
+      </div>
+   );
+};
+
+export default Fila3Focos;
